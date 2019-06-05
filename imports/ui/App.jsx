6acc0487b5897258ad1df6bcda24af676d50e9e5
harrychuang <vrjgik5@gmail.com>
2019-06-04 20:08:03 -0700

    added component in App.jsx for testing

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import Map from './components/MapView';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />
+    <Map />>
   </div>
 );
 
