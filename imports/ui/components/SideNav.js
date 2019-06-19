d8643deb9a5cb4122b3fee74d4be54e20a1b2e51
harrychuang <vrjgik5@gmail.com>
2019-06-18 20:22:51 -0700

    simplified sideNav as it does not need a reducer

--- imports/ui/components/SideNav.js
+++ imports/ui/components/SideNav.js
@@ -1,9 +1,7 @@
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_sidenav_fixed2
 import React from 'react';
-import {connect} from 'react-redux';
 
 class SideNav extends React.Component {
-    // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_sidenav_fixed2
-
     render() {
         return (
             <div className="sidenav">
@@ -12,9 +10,4 @@ class SideNav extends React.Component {
         );
     }
 }
-
-const mapStateToProps = (state) => {
-    return state;
-};
-
-export default connect(mapStateToProps)(SideNav);
+export default SideNav;
