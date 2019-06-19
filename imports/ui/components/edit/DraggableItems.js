8819e4d9efdb6e3461ee5ed8bbff21b8f25ec221
harrychuang <vrjgik5@gmail.com>
2019-06-19 01:10:19 -0700

    styles the font size of the draggable item

--- imports/ui/components/edit/DraggableItems.js
+++ imports/ui/components/edit/DraggableItems.js
@@ -2,17 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {updateDraggableItems} from "../../actions/draggableItemsActions";
 import {VanGoStore} from "../../../../client/main";
-
-
-
-
-
+import {maskString} from "../../../util/util";
 
 
 class DraggableItems extends React.Component {
-    state = {
-        items: ["🍰 EventA", "🍩 EventB", "🍎 AttractionC", "🍕 AttractionD", "🐵 AttractionE", "🥎 AttractionF"]
-    };
+    // state = {
+    //     items: ["🍰 EventA", "🍩 EventB", "🍎 AttractionC", "🍕 AttractionD", "🐵 AttractionE", "🥎 AttractionF"]
+    // };
 
     componentDidMount() {
         let eventsFromHomePage = VanGoStore.getState().eventDrawer.savedEvents;
@@ -20,14 +16,14 @@ class DraggableItems extends React.Component {
     }
 
     onDragStart = (e, index) => {
-        this.draggedItem = this.state.items[index];
+        this.draggedItem = VanGoStore.getState().draggableItems.editedPath[index];
         e.dataTransfer.effectAllowed = "move";
         e.dataTransfer.setData("text/html", e.target.parentNode);
         e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
     };
 
     onDragOver = index => {
-        const draggedOverItem = this.state.items[index];
+        const draggedOverItem = VanGoStore.getState().draggableItems.editedPath[index];
 
         // if the item is dragged over itself, ignore
         if (this.draggedItem === draggedOverItem) {
@@ -35,48 +31,58 @@ class DraggableItems extends React.Component {
         }
 
         // filter out the currently dragged item
-        let items = this.state.items.filter(item => item !== this.draggedItem);
+        let items = VanGoStore.getState().draggableItems.editedPath.filter(item => item !== this.draggedItem);
 
         // add the dragged item after the dragged over item
         items.splice(index, 0, this.draggedItem);
-
-        this.setState({ items });
+        this.props.updateDraggableItems(items);
     };
 
     onDragEnd = () => {
         this.draggedIdx = null;
     };
 
+    displayItems = (items) => {
+        return items.map((item, idx) => (
+            <li key={item.id} onDragOver={() => this.onDragOver(idx)}>
+                <div
+                    className="drag"
+                    draggable
+                    onDragStart={e => this.onDragStart(e, idx)}
+                    onDragEnd={this.onDragEnd}
+                >
+                    <div className="ui label blue DraggableItem">
+                        <i className="arrows alternate vertical icon"/>
+                        <span className="content">{maskString(item.name, 23)}</span>
+                    </div>
+                </div>
+            </li>
+        ));
+    };
+
     /*
     * The content inside the <main> element
     * should be unique to the document.
     * */
-
     render() {
+        let items = VanGoStore.getState().draggableItems.editedPath;
+        if (items) {
+            return (
+                <div className="DraggableItems">
+                    <main>
+                        <ul>
+                            {this.displayItems(items)}
+                        </ul>
+                    </main>
+                </div>
+            );
+
+        }
+
         return (
             <div className="DraggableItems">
-                <main>
-                    <ul className={""}>
-                        {this.state.items.map((item, idx) => (
-                            <li key={item} onDragOver={() => this.onDragOver(idx)}>
-                                <div
-                                    className="drag"
-                                    draggable
-                                    onDragStart={e => this.onDragStart(e, idx)}
-                                    onDragEnd={this.onDragEnd}
-                                >
-
-
-                                    <div className="ui label huge blue">
-                                        <label className="dragBtn"> ≡ </label>
-                                        <span className="content">{item}</span>
-                                    </div>
-                                </div>
-
-                            </li>
-                        ))}
-                    </ul>
-                </main>
+                NO ITEMS SAVED IN EVENT DRAWER. <br/>
+                Please go back to homepage and save your events.
             </div>
         );
     }
