00523f8682f5deaa4adfb4139f43e6882c4daae7
harrychuang <vrjgik5@gmail.com>
2019-07-04 19:16:54 -0700

    created function to accommodate user in EvenDrawer. Not tested yet

--- imports/api/EventDrawerApi.js
+++ imports/api/EventDrawerApi.js
@@ -8,6 +8,55 @@ import {Meteor} from 'meteor/meteor';
 const EventDrawerApi = new Mongo.Collection('eventDrawer');
 
 
+const getAnonAccountID = async () => {
+    let anonDrawer = EventDrawerApi.findOne({user: 'anon'});
+    if (!anonDrawer){
+        let anonAccount = {
+            user: 'anon',
+            items:[]
+        };
+        let _id = await EventDrawerApi.insert(anonAccount);
+        return _id;
+    }
+    return anonDrawer._id;
+};
+
+const updateAnonAccount = async (item) => {
+    try {
+        let anonDrawerID = getAnonAccountID();
+        let anonDrawer = EventDrawerApi.findOne({_id: anonDrawerID});
+        let updatedDrawer = anonDrawer.items.push(item);
+        return await EventDrawerApi.update({_id: anonDrawerID}, {updatedDrawer});
+    } catch (err) {
+        console.log(`Error occurred when saving to anonymous account: ${err.message}`);
+    }
+};
+
+const getUserAccountID = async () => {
+    let userDrawerID = EventDrawerApi.findOne({user: Meteor.userId()});
+    if (!userDrawerID){
+        let userAccount = {
+            user: Meteor.userId(),
+            items:[]
+        };
+        let _id = await EventDrawerApi.insert(userAccount);
+        return _id;
+    }
+    return userDrawerID._id;
+
+};
+
+const updateExistingAccount = async (item) => {
+    let userDrawerID = getUserAccountID();
+    try {
+        let userDrawer = EventDrawerApi.findOne({_id: userDrawerID});
+        let updatedDrawer = userDrawer.items.push(item);
+        return await EventDrawerApi.update({_id: userDrawerID}, {updatedDrawer});
+    } catch (err) {
+        console.log(`Error occurred when saving to account ${userDrawerID}: ${err.message}`);
+    }
+};
+
 
 // TODO: This will need some restrictions
 if (Meteor.isServer) {
