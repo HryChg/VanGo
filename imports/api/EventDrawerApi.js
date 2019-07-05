6f63984a5523738c777f716c0b9254dc20eac701
harrychuang <vrjgik5@gmail.com>
2019-07-04 18:43:41 -0700

    able to access who is the current userID, userName, userEmail

--- imports/api/EventDrawerApi.js
+++ imports/api/EventDrawerApi.js
@@ -21,6 +21,8 @@ if (Meteor.isServer) {
         saveEventToDrawer: async (eventToBeSaved) => {
             try {
                 console.log(`the current user id is ${Meteor.userId()}`);
+                console.log(`the current user first email is ${Meteor.user().emails[0].address}`);
+                console.log(`the current user name is ${Meteor.user().profile.name}`);
 
                 return await EventDrawerApi.insert(eventToBeSaved);
             } catch (err) {
