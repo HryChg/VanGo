2f6bb64b834b2bf20011cbd8fc7fedd6a8db5eef
harrychuang <vrjgik5@gmail.com>
2019-07-05 17:14:02 -0700

    added documentation on testing

--- imports/api/sample.tests.js
+++ imports/api/sample.tests.js
@@ -1,6 +1,10 @@
+// Testing Tutorial
+// https://www.youtube.com/watch?v=ISuVRl21wUs
+
+// Meteor + React Testing
+// https://www.meteor.com/tutorials/react/testing
+
 import assert from "assert";
-
-
 describe("Sample Testing for Api Files", function () {
     it("Testing for Api", async function () {
         assert.strictEqual("VanGo", "VanGo");
