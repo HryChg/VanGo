6b77eb595e0960ab643f27039f3645f0cbbea22d
harrychuang <vrjgik5@gmail.com>
2019-07-05 17:12:16 -0700

    set up sample files for testing!

---
+++ imports/api/sample.tests.js
@@ -0,0 +1,8 @@
+import assert from "assert";
+
+
+describe("Sample Testing for Api Files", function () {
+    it("Testing for Api", async function () {
+        assert.strictEqual("VanGo", "VanGo");
+    });
+});
