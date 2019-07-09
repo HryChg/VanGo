9193a2d9597e24db01fa76ed28701263a220094e
shirleywbi <swwbi@msn.com>
2019-07-08 22:54:17 -0700

    add delete and update meteor methods for itinerary

--- server/methods/itinerary.js
+++ server/methods/itinerary.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import Itineraries from '../../imports/api/itineraries';
 
 Meteor.methods({
+    // EFFECTS: adds itinerary to collection; throws error if not logged in
     'saveItinerary': function(itinerary) {
         if (!Meteor.userId()) {
             const err = 'Not Authorized: Must be logged in to save itinerary.';
@@ -14,7 +15,25 @@ Meteor.methods({
             date: itinerary.date,
             user: Meteor.userId()
         }, (err) => {
-            if (err) console.log(err);
+            if (err) throw new Meteor.Error(err, err);
         });
+    },
+    // EFFECTS: deletes an itinerary with given id; does nothing if ID not found
+    'deleteItinerary': function(id) {
+        Itineraries.remove({
+            _id: id
+        }, (err) => {
+            if (err) throw new Meteor.Error(err, err);
+        })
+    },
+    // Template for an update
+    'updateItinerary': function(id) {
+        Itineraries.update({
+            _id: id
+        }, {
+            // What needs to be updated
+        }, (err) => {
+            if (err) throw new Meteor.Error(err, err);
+        })
     }
 });
