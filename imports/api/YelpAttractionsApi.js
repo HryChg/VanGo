59958652aabffbca5ff70a5af64fc2ce8208d509
harrychuang <vrjgik5@gmail.com>
2019-07-09 17:09:01 -0700

    got rid of unnecessary code in YelpAttractionApi

--- imports/api/YelpAttractionsApi.js
+++ imports/api/YelpAttractionsApi.js
@@ -47,21 +47,6 @@ export default class YelpAttractionsApi {
     }
 }
 
-approximatePrice = (dollarSign) => {
-    switch (dollarSign) {
-        case '$':
-            return 10;
-        case '$$':
-            return 30;
-        case '$$$':
-            return 60;
-        case '$$$$':
-            return 100;
-        default:
-            return 0;
-    }
-}
-
 export const convertBusinessToAttraction = (business) => {
     let attraction = {};
     attraction.name = business.name;
