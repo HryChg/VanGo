751813e7d3e71eb0bd4b2b1010b5c4fe5886387f
mdatan <marydatan@outlook.com>
2019-07-16 15:36:32 -0700

    update events meteor method in date picker

--- imports/api/CurrentEvents.js
+++ imports/api/CurrentEvents.js
@@ -1,17 +1,31 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
+// import getDayEvents from './clientEvents';
+import getEventsInDay from './clientEvents';
 
 const CurrentEvents = new Mongo.Collection('currentEvents');
 
 // TODO: This will need some restrictions
 if (Meteor.isServer) {
-    Meteor.publish('currentEvents', function() {
+    Meteor.publish('currentEvents', function () {
         return CurrentEvents.find();
-    })
+    });
+
+    Meteor.methods({
+        'updateEvents': async (date) => {
+            CurrentEvents.remove({});
+            var newEvents = await getEventsInDay(date);
+            for (event of newEvents.events) {
+                CurrentEvents.insert(event)
+            }
+        }
+    });
 }
 
 if (Meteor.isClient) {
     Meteor.subscribe('currentEvents');
 }
+
+export { updateEvents };
 
 export default CurrentEvents;
