77750b5a5745653f5f4802d1db0fa0ac9fa95fad
harrychuang <vrjgik5@gmail.com>
2019-07-18 15:31:18 -0700

    created a class for mailgun, in the process of testing it

--- server/methods/mailgun.js
+++ server/methods/mailgun.js
@@ -1,10 +1,54 @@
 // Link to Combine MailGun and NodeMailer
 // https://www.youtube.com/watch?v=i62jmLC15qQ
 
+import nodemailer from 'nodemailer';
+import nodemailMailgun from 'nodemailer-mailgun-transport';
+
+
 import {mailgunAuth} from "../../imports/ui/config";
 
-const nodemailer = require('nodemailer');
-const nodemailMailgun = require('nodemailer-mailgun-transport');
+
+// const nodemailer = require('nodemailer');
+// const nodemailMailgun = require('nodemailer-mailgun-transport');
+
+export default class MailGun {
+    constructor(){
+        let auth = {
+            auth: {
+                api_key: mailgunAuth.api_key,
+                domain: mailgunAuth.domain,
+            }
+        };
+        this.transporter = nodemailer.createTransport(nodemailMailgun(auth));
+    }
+    setMailOptions(from, to, subject, html){
+        this.mailOptions = {
+            from,
+            to,
+            subject,
+            html
+        }
+    }
+    sendMail(){
+        this.transporter.sendMail(this.mailOptions, (err, data)=>{
+            if (err){
+                console.log(err);
+            } else {
+                console.log(`Message Sent to ${this.mailOptions.to}`);
+            }
+        });
+    }
+    sendEmail (from, to, subject, html){
+        this.setMailOptions(from, to, subject, html);
+        this.sendEmail();
+    }
+}
+
+
+
+
+
+
 
 const auth = {
     auth: {
