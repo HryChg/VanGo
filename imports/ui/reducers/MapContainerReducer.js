87831eaa70cded32e85d61b04a2241e160e3a4f7
shirleywbi <swwbi@msn.com>
2019-07-25 15:55:07 -0700

    updated map container reducer with change date case to hide info window when new date

--- imports/ui/reducers/MapContainerReducer.js
+++ imports/ui/reducers/MapContainerReducer.js
@@ -6,34 +6,43 @@ let initialState = {
 };
 
 export default function MapContainerReducer(state = initialState, action) {
-    if (action.type === 'ON_MARKER_CLICK') {
-        return {
-            mapLoaded: state.mapLoaded,
-            showingInfoWindow: action.payload.showingInfoWindow,
-            activeMarker: action.payload.activeMarker,
-            selectedPlace: action.payload.selectedPlace // A Google Map Marker Prop
-        }
-    } else if (action.type === 'ON_MAP_CLICKED') {
-        return {
-            mapLoaded: state.mapLoaded,
-            showingInfoWindow: false,
-            activeMarker: null,
-            selectedPlace: state.selectedPlace
-        }
-    } else if (action.type === 'POP_UP_INFO_WINDOW'){
-        return {
-            mapLoaded: state.mapLoaded,
-            showingInfoWindow: action.payload.showingInfoWindow,
-            activeMarker: action.payload.activeMarker,
-            selectedPlace: action.payload.selectedPlace
-        }
-    } else if (action.type === 'SET_MAP_LOADED_TRUE'){
-        return {
-            mapLoaded: true,
-            showingInfoWindow: state.showingInfoWindow,
-            activeMarker: state.activeMarker,
-            selectedPlace: state.selectedPlace
-        }
+    switch(action.type) {
+        case 'ON_MARKER_CLICK':
+            return {
+                mapLoaded: state.mapLoaded,
+                showingInfoWindow: action.payload.showingInfoWindow,
+                activeMarker: action.payload.activeMarker,
+                selectedPlace: action.payload.selectedPlace // A Google Map Marker Prop
+            }
+        case 'ON_MAP_CLICKED':
+            return {
+                mapLoaded: state.mapLoaded,
+                showingInfoWindow: false,
+                activeMarker: null,
+                selectedPlace: state.selectedPlace
+            }
+        case 'POP_UP_INFO_WINDOW':
+            return {
+                mapLoaded: state.mapLoaded,
+                showingInfoWindow: action.payload.showingInfoWindow,
+                activeMarker: action.payload.activeMarker,
+                selectedPlace: action.payload.selectedPlace
+            }
+        case 'SET_MAP_LOADED_TRUE':
+            return {
+                mapLoaded: true,
+                showingInfoWindow: state.showingInfoWindow,
+                activeMarker: state.activeMarker,
+                selectedPlace: state.selectedPlace
+            }
+        case 'CHANGE_DATE':
+            return {
+                mapLoaded: state.mapLoaded,
+                showingInfoWindow: false,
+                activeMarker: state.activeMarker,
+                selectedPlace: state.selectedPlace
+            }
+        default:
+            return state;
     }
-    return state;
 }
