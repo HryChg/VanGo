ed55ea689b2ffd17056d54501ef5c4c3fa29829a
shirleywbi <swwbi@msn.com>
2019-07-29 20:21:48 -0700

    update price range filter to [0,0], no longer sticky

--- imports/ui/components/home/EventFilter.js
+++ imports/ui/components/home/EventFilter.js
@@ -105,7 +105,7 @@ class EventFilter extends React.Component {
                                 to
                             <Input placeholder="Enter Value" onChange={(e) => {this.props.filterPriceByEntry(e)}} /> */}
                             <Slider multiple color="red" settings={{
-                                start: [-1,0],
+                                start: [0,0],
                                 min: 0,
                                 max: this.getMaxPrice() + 10,
                                 step: 1,
