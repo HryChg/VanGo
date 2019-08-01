3c7614ce428e9759180ad322a546aed51a6efd85
harrychuang <vrjgik5@gmail.com>
2019-08-01 16:05:31 -0700

    Meteor Methods in MailgunEmailStats are tested

--- imports/api/MailgunEmailStats.js
+++ imports/api/MailgunEmailStats.js
@@ -6,11 +6,12 @@ const MailgunEmailStats = new Mongo.Collection('MailgunEmailStats');
 const MAX_EMAILS_PER_MONTH = 10000;
 
 const getUserEmailStats = async () => {
-    let stats = await MailgunEmailStats.findOne({userId: Meteor.userId()});
-    console.log(`userID: ${Meteor.userId()}`);
+    let userId = Meteor.userId();
+    let stats = await MailgunEmailStats.findOne({userId: userId});
     if (!stats) {
-        let stats = {userId: Meteor.userId(), emailCount: 0};
-        return await MailgunEmailStats.insert(stats);
+        let stats = {userId: userId, emailCount: 0};
+        let temp = await MailgunEmailStats.insert(stats);
+        return await MailgunEmailStats.findOne({_id: temp})
     }
     return stats;
 };
@@ -28,7 +29,7 @@ Meteor.methods({
     getUserEmailStats: getUserEmailStats,
 
     addToUserEmailStats: async () => {
-        let stats = await getUserEmailStats();
+        let stats = await getUserEmailStats(Meteor.userId());
         stats.emailCount += 1;
         return await MailgunEmailStats.update({userId: stats.userId}, stats);
     },
