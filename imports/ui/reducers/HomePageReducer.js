452bd582fd081fec166ef03900bedb3bd15d0839
harrychuang <vrjgik5@gmail.com>
2019-08-01 11:48:54 -0700

    implemented dimmer state in homePageReducer

--- imports/ui/reducers/HomePageReducer.js
+++ imports/ui/reducers/HomePageReducer.js
@@ -9,6 +9,16 @@ export default function HomePageReducer(state = initState, action) {
             toggleNearbyAttractions: !state.toggleNearbyAttractions,
             dimmerActive: state.dimmerActive
         }
+    } else if (action.type === "SHOW_DIMMER"){
+        return {
+            toggleNearbyAttractions: state.toggleNearbyAttractions,
+            dimmerActive: true
+        }
+    } else if (action.type === "HIDE_DIMMER"){
+        return {
+            toggleNearbyAttractions: state.toggleNearbyAttractions,
+            dimmerActive: false
+        }
     }
     return state;
 }
