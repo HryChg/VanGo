f29418fa9f041b0237a03be84be5df2d73cc8aa8
mdatan <marydatan@outlook.com>
2019-08-05 16:59:16 -0700

    added space in link to website

--- server/main.js
+++ server/main.js
@@ -11,15 +11,6 @@ import YelpAttractionsApi, {convertBusinessesToAttractions} from "../imports/api
 
 
 Meteor.startup(async () => {
-    // CurrentEvents.remove({});
-    // console.log(`clear out current events collection`);
-
-    // let eventsToday = await getEventsInDay(new Date());
-    // for (event of eventsToday.events) {
-    //     CurrentEvents.insert(event);
-    // }
-    // console.log(`Added ${eventsToday.events.length} events from Yelp`);
-
     let yelp = new YelpAttractionsApi();
     let res = await yelp.getTouristAttractionFromCoord(50, 49.2820, -123.1171);
     let attractions = convertBusinessesToAttractions(res);
@@ -27,11 +18,6 @@ Meteor.startup(async () => {
         Attractions.insert(attraction);
     }
     console.log(`Added ${attractions.length} attraction from Yelp`);
-
-    // Meteor.call('getUserEmailStats', (err, res)=>{
-    //     console.log(err);
-    //     console.log(res);
-    // })
 });
 
 
